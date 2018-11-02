79483c6946369133345ea13cf11a1c583919e018
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-11-02 11:03:07 +0530

    eval try catch

--- umd/src/AmdLoader.ts
+++ umd/src/AmdLoader.ts
@@ -249,7 +249,7 @@ AmdLoader.moduleLoader = (name, url, success, error) => {
                 success(() => {
 
                     const errorCheck: string = `
-} catch(e) { if(e.stack) { alert(e.message + '\r\n' + e.stack); } else { alert(e); } }`;
+} catch(e) { if(e.stack) { alert(e.message + '\\r\\n' + e.stack); } else { alert(e); } }`;
 
                     // tslint:disable-next-line:no-eval
                     eval(`"use strict"; try { ${xhr.responseText} ${errorCheck}

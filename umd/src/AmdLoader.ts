e17e1ca12b8545188096aab5867c1b12b7035a9a
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-01-18 18:53:46 +0530

    wip

--- umd/src/AmdLoader.ts
+++ umd/src/AmdLoader.ts
@@ -369,6 +369,8 @@ class AmdLoader {
                     resolve();
 
                 } catch (e) {
+                    // tslint:disable-next-line: no-console
+                    console.error(e);
                     reject(e);
                 }
 

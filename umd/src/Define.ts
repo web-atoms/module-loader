02a584c1a278dc8c3e7bb1ba7672ce1d89917889
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-03-28 13:40:08 +0530

    Added packed loader

--- umd/src/Define.ts
+++ umd/src/Define.ts
@@ -1,16 +1,71 @@
 /// <reference path="./AmdLoader.ts"/>
 
+type IAnyFunction = (... a: any[]) => any;
+
 // tslint:disable-next-line
 interface IDefine {
-    (requiresOrFactory: string[] | (() => void ),
-     factory?: (r: any, e: any) => void): void;
+    (requiresOrFactory: string[] | IAnyFunction,
+     factory?: IAnyFunction);
     amd?: object;
 }
 
 // tslint:disable-next-line:no-var-keyword
 var define: IDefine = (
-    requiresOrFactory: string[] | (() => void ),
-    factory: (r: any, e: any) => void): void => {
+    requiresOrFactory: string[] | IAnyFunction,
+    factory: IAnyFunction,
+    nested?: boolean): void => {
+
+    const loader = AmdLoader.instance;
+
+    function bindFactory(module: Module, requireList: string[], fx: IAnyFunction) {
+        if (module.factory) {
+            return;
+        }
+        module.dependencies = [];
+        let requires: string[] = requireList;
+        requires = requireList;
+
+        const args: any[] = [];
+        for (const s of requires) {
+            if (s === "require") {
+                args.push(module.require);
+                continue;
+            }
+            if (s === "exports") {
+                args.push(module.emptyExports);
+                continue;
+            }
+            if (/^global/.test(s)) {
+                args.push(loader.get(s).exports);
+            }
+            const name: string = loader.resolveRelativePath(s, module.name);
+            const child: Module = loader.get(name);
+            module.addDependency(child);
+        }
+        module.factory = () => {
+            return fx.apply(module, args);
+        };
+    }
+
+    if (nested) {
+        // this means this was executed as packed modules..
+        // first parameter is name, second is array and third is factory...
+
+        const name = requiresOrFactory as unknown as string;
+        const rList = factory as unknown as string[];
+        const f = nested as unknown as () => void;
+
+        const module = AmdLoader.instance.get(name);
+
+        bindFactory(module, rList, f);
+
+        // we must call load modules after this..
+        setTimeout(() => {
+            loader.loadDependencies(module);
+        }, 1);
+
+        return;
+    }
 
     AmdLoader.instance.define = () => {
         const current: Module = AmdLoader.current;
@@ -20,38 +75,11 @@ var define: IDefine = (
         if (current.factory) {
             return;
         }
-        // console.log(`Define for ${current.name}`);
-        current.dependencies = [];
-        const loader: AmdLoader = AmdLoader.instance;
-
-        let requires: string[] = [];
         if (typeof requiresOrFactory === "function") {
-            factory = requiresOrFactory;
+            bindFactory(current, [], requiresOrFactory);
         } else {
-            requires = requiresOrFactory;
+            bindFactory(current, requiresOrFactory, factory);
         }
-
-        const args: any[] = [];
-        for (const s of requires) {
-            if (s === "require") {
-                args.push(current.require);
-                continue;
-            }
-            if (s === "exports") {
-                args.push(current.emptyExports);
-                continue;
-            }
-            if (/^global/.test(s)) {
-                args.push(loader.get(s).exports);
-            }
-            const name: string = loader.resolveRelativePath(s, current.name);
-            const child: Module = loader.get(name);
-            current.addDependency(child);
-        }
-        // const fx = factory.bind(current, ... args);
-        current.factory = () => {
-            return factory.apply(current, args);
-        };
     };
 };
 

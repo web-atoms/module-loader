abef9170ac8e18a8c64c70612ba2fb2bd72a5177
Akash Kava <39438041+ackava@users.noreply.github.com>
2021-10-31 18:30:39 +0530

    Added no check

--- umd/src/ReflectMetadata.ts
+++ umd/src/ReflectMetadata.ts
@@ -1,4 +1,5 @@
 // tslint:disable
+// @ts-nocheck
 /*! *****************************************************************************
 Copyright (C) Microsoft. All rights reserved.
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use
